0c0eb626494fccb8255a88c803e90d02b7c6749e
Giorgio Comai <g@giorgiocomai.eu>
2026-03-14 09:13:27 +0100

    add omekasr from codeberg

--- packages.R
+++ packages.R
@@ -1,21 +1,33 @@
 packages_df <- tibble::tribble(
-  ~package, ~url,
-  "ganttrify", "https://github.com/giocomai/ganttrify",
-  "castarter", "https://github.com/giocomai/castarter",
-  "rbackupr", "https://github.com/giocomai/rbackupr",
-  "nomnomlgraph", "https://github.com/giocomai/nomnomlgraph",
-  "latlon2map", "https://github.com/giocomai/latlon2map",
-  "cornucopia", "https://github.com/giocomai/cornucopia/",
+  ~package,
+  ~url,
+  "ganttrify",
+  "https://github.com/giocomai/ganttrify",
+  "castarter",
+  "https://github.com/giocomai/castarter",
+  "rbackupr",
+  "https://github.com/giocomai/rbackupr",
+  "nomnomlgraph",
+  "https://github.com/giocomai/nomnomlgraph",
+  "latlon2map",
+  "https://github.com/giocomai/latlon2map",
+  "cornucopia",
+  "https://github.com/giocomai/cornucopia/",
+  "omekasr",
+  "https://codeberg.org/giocomai/omekasr/",
   # LLM
-  "quackingllama", "https://github.com/giocomai/quackingllama",
+  "quackingllama",
+  "https://github.com/giocomai/quackingllama",
   # api wrappers
-  "zoteror", "https://github.com/giocomai/zoteror",
-  "plausibler", "https://github.com/giocomai/plausibler",
-  "huecontroller", "https://github.com/giocomai/huecontroller",
+  "zoteror",
+  "https://github.com/giocomai/zoteror",
+  "plausibler",
+  "https://github.com/giocomai/plausibler",
+  "huecontroller",
+  "https://github.com/giocomai/huecontroller",
   # wrappers for command line tools
-  "ytdlpr", "https://github.com/giocomai/ytdlpr"
+  "ytdlpr",
+  "https://github.com/giocomai/ytdlpr"
 )
 
-jsonlite::write_json(packages_df,
-                     'packages.json',
-                     pretty = TRUE)
+jsonlite::write_json(packages_df, 'packages.json', pretty = TRUE)
